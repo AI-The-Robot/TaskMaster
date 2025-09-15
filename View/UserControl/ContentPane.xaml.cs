50bf46a5abee8b00b0c1c1cfe26168cb153503a9
Ayotunde Ismail <emphaticTradition6@outlook.com>
2025-09-15 17:33:39 +0100

    Deleted the previous code to restart the project

--- View/UserControl/ContentPane.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace WPFProject.View.UserControl;
-
-public partial class ContentPane : System.Windows.Controls.UserControl
-{
-    public ContentPane()
-    {
-        InitializeComponent();
-    }
-}
---
+++ View/UserControl/TextBox.xaml.cs
@@ -0,0 +1,9 @@
+namespace WPFProject.View.UserControl;
+
+public partial class TextBox : System.Windows.Controls.UserControl
+{
+    public TextBox()
+    {
+        InitializeComponent();
+    }
+}
